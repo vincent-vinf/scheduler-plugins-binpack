52902616843dda45eaec6364c443f9fe06b00590
Vincent <857618786vin@gmail.com>
2025-03-03 15:19:24 +0800

    feat: binpack plugin

---
+++ pkg/binpack/binpack.go
@@ -0,0 +1,83 @@
+package binpack
+
+import (
+	"context"
+	"fmt"
+	"math"
+
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog/v2"
+	"k8s.io/kubernetes/pkg/scheduler/framework"
+)
+
+type BinPack struct {
+	handle framework.Handle
+}
+
+var _ = framework.ScorePlugin(&BinPack{})
+
+// Name is the name of the plugin used in the Registry and configurations.
+const Name = "Binpack"
+
+func (bp *BinPack) Name() string {
+	return Name
+}
+
+// NormalizeScore 将分数规整到框架支持的分数区间
+func (bp *BinPack) NormalizeScore(ctx context.Context, state *framework.CycleState, p *v1.Pod, scores framework.NodeScoreList) *framework.Status {
+	// Find highest and lowest scores.
+	var highest int64 = -math.MaxInt64
+	var lowest int64 = math.MaxInt64
+	for _, nodeScore := range scores {
+		if nodeScore.Score > highest {
+			highest = nodeScore.Score
+		}
+		if nodeScore.Score < lowest {
+			lowest = nodeScore.Score
+		}
+	}
+
+	// Transform the highest to lowest score range to fit the framework's min to max node score range.
+	oldRange := highest - lowest
+	newRange := framework.MaxNodeScore - framework.MinNodeScore
+	for i, nodeScore := range scores {
+		if oldRange == 0 {
+			scores[i].Score = framework.MinNodeScore
+		} else {
+			scores[i].Score = ((nodeScore.Score - lowest) * newRange / oldRange) + framework.MinNodeScore
+		}
+	}
+
+	return nil
+}
+
+// Score invoked at the score extension point.
+func (bp *BinPack) Score(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) (int64, *framework.Status) {
+	nodeInfo, err := bp.handle.SnapshotSharedLister().NodeInfos().Get(nodeName)
+	if err != nil {
+		return 0, framework.NewStatus(framework.Error, fmt.Sprintf("getting node %q from Snapshot: %v", nodeName, err))
+	}
+	return bp.score(nodeInfo)
+}
+
+func (bp *BinPack) score(nodeInfo *framework.NodeInfo) (int64, *framework.Status) {
+	// 剩余可分配量 = 总可分配量 - 已分配量
+	rest := nodeInfo.Allocatable.MilliCPU - nodeInfo.Requested.MilliCPU
+	score := -rest
+	// 根据rest计算分数，剩余越多分数越低
+	klog.Infof("node %s get score %d", nodeInfo.Node().Name, score)
+
+	return score, nil
+}
+
+// ScoreExtensions of the Score plugin.
+func (bp *BinPack) ScoreExtensions() framework.ScoreExtensions {
+	return bp
+}
+
+func New(args runtime.Object, h framework.Handle) (framework.Plugin, error) {
+	return &BinPack{
+		handle: h,
+	}, nil
+}
